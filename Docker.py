c6255ad96a86461d5924df2916d31f490d7a7153
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2021-07-18 09:44:43 +0630

    Update Docker.py

--- Docker.py
+++ Docker.py
@@ -6,14 +6,14 @@ headers = {
 	}
 GS_url="https://www.google.com/search?q="
 
-Count=input("Enter country name::")
+Count=input("Enter country name or gov site::|>")
 
 print("[+]Choose Option[+]")
-print("[1]Docker(1)")
-print("[2]Docker(2)")
-print("[3]Docker(3)")
+print("    [1]Docker(1)")
+print("      [2]Docker(2)")
+print("        [3]Docker(3)")
 Dork=input("Enter your choice number::")
-
+fff=("id=")
 if Dork == "1" :
     list=[]
     D_url1="Inurl:/php?id=?"
@@ -21,13 +21,14 @@ if Dork == "1" :
     work1 = requests.get(W1_url, headers=headers)
     data=work1.text
     soup=BeautifulSoup(data, "html.parser")
-    for text in soup.find_all('a'):
+    for text in soup.find_all('a', href=True):
         list.append(text.get('href'))
     filter=input("Enter, what you want to filter word::|>")
     for txt in list:
         if filter in txt:
+            print("[++]Found[++]"
             print(txt)
-             
+            print("")
 
 else:
      print("Coming Soon")

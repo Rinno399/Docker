2dd600e3ee429f03f6dc70b357468ded069e1f7a
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2021-07-18 09:16:58 +0630

    Update Docker.py

--- Docker.py
+++ Docker.py
@@ -5,7 +5,6 @@ headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
 	}
 GS_url="https://www.google.com/search?q="
-list=[]
 
 Count=input("Enter country name::")
 
@@ -16,7 +15,7 @@ print("[3]Docker(3)")
 Dork=input("Enter your choice number::")
 
 if Dork == "1" :
-  
+    list=[]
     D_url1="Inurl:/php?id=?"
     W1_url=GS_url+D_url1+Count
     work1 = requests.get(W1_url, headers=headers)

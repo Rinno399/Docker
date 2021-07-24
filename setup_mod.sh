4929d8f0ca71191aea38d89d4c7e9589aa59af53
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2021-07-24 12:27:47 +0630

    Update setup_mod.sh

--- setup_mod.sh
+++ setup_mod.sh
@@ -1,13 +1,5 @@
-echo -e"\e[1;36m
-.---.  _                   
-: .; ::_;                  
-:   .'.-.,-.,-.,-.,-. .--. 
-: :.`.: :: ,. :: ,. :' .; :
-:_;:_;:_;:_;:_;:_;:_;`.__.'
-\e[0m"
-
-
-
+pkg install figlet
+figlet Rinno
 echo -e "\e[1;34[+++]Starting[+++]\e[0m"
 echo -e "\e[1;34Please wait,\e[0m"
 pkg install python3

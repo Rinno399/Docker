851cef794e68ccbc8a5161dfc292620b77690338
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2021-07-24 12:31:40 +0630

    Delete setup_mod.sh

--- setup_mod.sh
+++
@@ -1,7 +0,0 @@
-pkg install figlet
-figlet Rinno
-echo -e "\e[1;34[+++]Starting[+++]\e[0m"
-echo -e "\e[1;34Please wait,\e[0m"
-pkg install python3
-python3 -m pip install requests
-python3 -m pip install BeautifulSoup
